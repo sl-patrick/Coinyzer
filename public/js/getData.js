a5b79426d5fc11d2f2a32afabd1dbdf6cfed3921
Samé Lhori Patrick <sl.patrick@icloud.com>
2021-07-24 14:29:44 +0200

    evite l'actualisation de la page quand l'utilisateur appuie sur le bouton rafraichir.

--- public/js/getData.js
+++ public/js/getData.js
@@ -1,27 +1,18 @@
-// const apiKey = '252f639021da21a5be8d9e776b68725b83c70f76d7b3b037cbd23239f0aa0647';
-//Je crée un tableau vide pour y stocker tout les noms des cryptomonnaies.
-let data= [];
-let names = document.querySelectorAll('.name');
-// console.log(names);
-names.forEach(element => {
-    data.push(element.innerHTML.toUpperCase())
+let refreshButton = document.querySelector('.refresh');
+
+refreshButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			console.log(this.response);
+		}
+	};
+	xhr.open("POST", '/cryptocurrencies/refresh', true);
+	xhr.responseType = "json";
+	xhr.send();
 });
-//Je transforme le tableau en string
-data = data.join(",");
-// console.log(data);
 
-function async() {
-    fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${data}&tsyms=EUR&api_key=${apiKey}`)
-    .then((response) => response.json())
-    .then(function(data) {
-        // return data.RAW;
-        let array = data.RAW;
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
 
-}
 
-async();
 
