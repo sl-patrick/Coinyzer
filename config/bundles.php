548ed5ce316e4e206c403ca3c78db81a45c70ba4
Samé Lhori Patrick <sl.patrick@icloud.com>
2021-07-27 13:35:59 +0200

    ajout du bundle KnpPaginator

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
