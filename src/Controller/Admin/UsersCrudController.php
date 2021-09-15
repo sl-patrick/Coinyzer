a9f1ae20254255a8dc192d1603d476f994487d3b
Samé Lhori Patrick <sl.patrick@icloud.com>
2021-09-15 14:48:39 +0200

    retrait de toutes les méthodes pour modifier le mot de passe.

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -3,27 +3,18 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Users;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
-use Symfony\Component\Form\FormBuilderInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
+
+
 class UsersCrudController extends AbstractCrudController
 {
-    /**
-     * @var UserPasswordEncoderInterface
-     */
-    private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
         $this->passwordEncoder = $passwordEncoder;
@@ -33,56 +24,11 @@ class UsersCrudController extends AbstractCrudController
     {
         return Users::class;
     }
-
     
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('pseudo'),
-            EmailField::new('email'),
-            //création des champs pour le nouveau mot de passe
-            Field::new('plainPassword', 'New password')
-                ->onlyOnForms()
-                ->setFormType(RepeatedType::class)
-                ->setFormTypeOptions([
-                    'type' => PasswordType::class,
-                    'first_options' => ['label' => 'Nouveau Mot de passe'],
-                    'second_options' => ['label' => 'Confirmez le Mot de passe']
-                ])->setRequired(true),
-            BooleanField::new('isVerified')->setFormTypeOptionIfNotSet('label_attr.class', 'switch-custom')->hideOnForm()
+            BooleanField::new('isVerified')->setFormTypeOptionIfNotSet('label_attr.class', 'switch-custom')->hideOnForm()->setPermission('ROLE_ADMIN')
         ];
     }
-
-    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
-    {
-        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
-        
-        $this->addEncodePasswordEventListener($formBuilder);
-        
-        return $formBuilder;
-        
-    }
-
-    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
-    {
-        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
-        
-        $this->addEncodePasswordEventListener($formBuilder);
-
-        return $formBuilder;
-        
-    }
-
-    public function addEncodePasswordEventListener(FormBuilderInterface $formBuilder)
-    {
-        $formBuilder->addEventListener(
-            FormEvents::SUBMIT, 
-            function(FormEvent $event) {
-                $user = $event->getData();
-                if ($user->getPlainPassword()) {
-                    $user->setPassword($this->passwordEncoder->encodePassword($user, $user->getPlainPassword()));
-                }
-            });
-    }
-    
 }
