683a9969edac4182e3ce9e85dea92149d141952c
Samé Lhori Patrick <sl.patrick@icloud.com>
2021-09-28 19:17:57 +0200

    Modification du role.

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -97,7 +97,7 @@ class Users implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_ADMIN';
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
